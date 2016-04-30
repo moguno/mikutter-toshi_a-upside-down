20507db9061b8a9e4282c31d7a59795de510926a
moguno <moguno@0kn.sakura.ne.jp>
2016-04-30 18:46:21 +0900

    としぁさんのツイートが読みにくいからひっくり返すコマンドを作りました。

--- mikutter-toshi_a-upside-down.rb
+++ mikutter-toshi_a-upside-down.rb
@@ -5,7 +5,7 @@ Plugin.create(:"mikutter-toshi_a-upside-down") {
     alias :render_to_context_org :render_to_context
 
     def render_to_context(context)
-      if message.user[:idname] == "toshi_a"
+      if message.user[:idname] =~ /toshi_a.*/ && !message[:teoku_nai]
         context.rotate(Math::PI)
         context.translate(-width, -height)
       end
@@ -13,4 +13,16 @@ Plugin.create(:"mikutter-toshi_a-upside-down") {
       render_to_context_org(context)
     end
   end
+
+  command(:teokurete_nai_nintei,
+          :name => _("逆さまにする"),
+          :condition => lambda { |opt| Plugin::Command[:HasMessage] && opt.messages[0].user[:idname] =~ /toshi_a.*/ },
+          :visible => true,
+          :role => :timeline) { |opt|
+    widget = Plugin[:gtk].widgetof(opt.widget)
+
+    opt.messages[0][:teoku_nai] = !opt.messages[0][:teoku_nai]
+
+    widget.modified(opt.messages[0])
+  }
 }
